b4648efedfad28df8088073d364c18b62a81a091
Arun Mahendrakar <Arun_Mahendrakar@Dell.com>
2021-09-14 08:01:03 +0530

    second chapter

---
+++ Chapter01Overview/Chapter02.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Concurrency.Chapter01Overview
+{
+    public class Chapter02
+    {
+        public async Task<int> FirstRespondingUrlAsync(string urlA, string urlB)
+        {
+            var httpClient = new HttpClient();
+            // Start both downloads concurrently.
+            Task<byte[]> downloadTaskA = httpClient.GetByteArrayAsync(urlA);
+            Task<byte[]> downloadTaskB = httpClient.GetByteArrayAsync(urlB);
+            // Wait for either of the tasks to complete.
+            Task<byte[]> completedTask = await Task.WhenAny(downloadTaskA, downloadTaskB);
+            // Return the length of the data retrieved from that URL.
+            
+            byte[] data = completedTask.Result;
+
+            if(downloadTaskA.Id == completedTask.Id)
+            {
+                Console.WriteLine("completed task a");
+            }
+            if(downloadTaskB.Id == completedTask.Id)
+            {
+                Console.WriteLine("completed task b");
+            }
+            
+            return data.Length;
+        }
+    }
+
+   
+}
--- Program.cs
+++ Program.cs
@@ -9,8 +9,25 @@ namespace Concurrency
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            await Chapter01.DoSomethingAsync();
-        }
 
+            Chapter02 chapter02 = new Chapter02();
+            int length = await chapter02.FirstRespondingUrlAsync("http://google.com", "http://timesofindia.com");
+            Console.WriteLine("length = {0}", length);
+
+            chapter02 = new Chapter02();
+            length = await chapter02.FirstRespondingUrlAsync("http://google.com", "http://timesofindia.com");
+            Console.WriteLine("length = {0}", length);
+
+            chapter02 = new Chapter02();
+            length = await chapter02.FirstRespondingUrlAsync("http://google.com", "http://timesofindia.com");
+            Console.WriteLine("length = {0}", length);
+
+            chapter02 = new Chapter02();
+            length = await chapter02.FirstRespondingUrlAsync("http://google.com", "http://timesofindia.com");
+            Console.WriteLine("length = {0}", length);
+
+
+
+        }
     }
 }
