1825d4b99474383e964d402da0b01b56bfe86290
Arun Mahendrakar <Arun_Mahendrakar@Dell.com>
2021-09-19 20:21:39 +0530

    chapter09

---
+++ Chapters/Chapter09.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concurrency.Chapters
+{
+    public class Chapter09 
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        private readonly HttpClient httpClient = new HttpClient
+        {
+            MaxResponseContentBufferSize = 1_000_000
+        };
+
+        static readonly IEnumerable<string> urlList = new string[]
+        {
+            "https://docs.microsoft.com",
+            "https://docs.microsoft.com/aspnet/core",
+            "https://docs.microsoft.com/azure",
+            "https://docs.microsoft.com/azure/devops",
+            "https://docs.microsoft.com/dotnet",
+            "https://docs.microsoft.com/dynamics365",
+            "https://docs.microsoft.com/education",
+            "https://docs.microsoft.com/enterprise-mobility-security",
+            "https://docs.microsoft.com/gaming",
+            "https://docs.microsoft.com/graph",
+            "https://docs.microsoft.com/microsoft-365",
+            "https://docs.microsoft.com/office",
+            "https://docs.microsoft.com/powershell",
+            "https://docs.microsoft.com/sql",
+            "https://docs.microsoft.com/surface",
+            "https://docs.microsoft.com/system-center",
+            "https://docs.microsoft.com/visualstudio",
+            "https://docs.microsoft.com/windows",
+            "https://docs.microsoft.com/xamarin"
+        };
+
+        public async Task CancellableTasks()
+        {
+            Console.WriteLine("Application started.");
+            Console.WriteLine("Press the ENTER key to cancel...\n");
+
+            Task cancelTask = Task.Run(() =>
+            {
+                while (Console.ReadKey().Key != ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Press the ENTER key to cancel...");
+                }
+
+                Console.WriteLine("\nENTER key pressed: cancelling downloads.\n");
+                cancellationTokenSource.Cancel();
+            });
+
+            Task sumPageSizesTask = SumPageSizesAsync();
+
+            await Task.WhenAny(new[] { cancelTask, sumPageSizesTask });
+
+            Console.WriteLine("Application ending.");
+        }
+
+        private async Task SumPageSizesAsync()
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            int total = 0;
+            try
+            {
+                foreach (string url in urlList)
+                {
+                    int contentLength = await ProcessUrlAsync(url, httpClient, cancellationTokenSource.Token);
+                    total += contentLength;
+                }
+            }
+            catch (OperationCanceledException oce)
+            {
+                Console.WriteLine(oce.Message);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"\nTotal bytes returned:  {total:#,#}");
+            Console.WriteLine($"Elapsed time:          {stopwatch.Elapsed}\n");
+        }
+
+        private async Task<int> ProcessUrlAsync(string url, HttpClient client, CancellationToken token)
+        {
+            if (token.IsCancellationRequested) 
+            {
+                throw new TaskCanceledException();
+            }
+            HttpResponseMessage response = await client.GetAsync(url, token);
+            byte[] content = await response.Content.ReadAsByteArrayAsync();
+            Console.WriteLine($"{url,-60} {content.Length,10:#,#}");
+
+            return content.Length;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,31 +3,36 @@ using Concurrency.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Concurrency
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            Chapter03 chapter03 = new Chapter03();
-            List<int> ints = new List<int> { 1, 2, 3, 4, 8, 21, 7, 15, 13, 7, 9 };
+            //Chapter03 chapter03 = new Chapter03();
+            //List<int> ints = new List<int> { 1, 2, 3, 4, 8, 21, 7, 15, 13, 7, 9 };
 
-            //chapter03.StoppingParallelForEach(ints);
+            ////chapter03.StoppingParallelForEach(ints);
 
-            //BinaryTree binaryTree = chapter03.PopulateTree();
-            //chapter03.ProcessTree(binaryTree.Root);
+            ////BinaryTree binaryTree = chapter03.PopulateTree();
+            ////chapter03.ProcessTree(binaryTree.Root);
 
-            List<int> output = chapter03.MultiplyBy2(ints);
-            output.ForEach(item => Console.Write(item + " "));
+            //List<int> output = chapter03.MultiplyBy2(ints);
+            //output.ForEach(item => Console.Write(item + " "));
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            output = chapter03.OrderedMultiplyBy2(ints);
-            output.ForEach(item => Console.Write(item + " "));
-            Console.WriteLine();
+            //output = chapter03.OrderedMultiplyBy2(ints);
+            //output.ForEach(item => Console.Write(item + " "));
+            //Console.WriteLine();
+
+            Chapter09 chapter09 = new Chapter09();
+            //await chapter09.CancellableTasks();
+            await chapter09.CancellOtherTaskwhenOneIsDone();
 
             Console.WriteLine("Done");
         }
